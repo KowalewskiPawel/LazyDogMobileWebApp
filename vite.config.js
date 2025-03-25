5d985862cd63d2ce4ccbe6b07a060cf0034d832e
poulou0 <poulou0@gmail.com>
2025-03-25 16:37:18 +0100

    feat: make host public

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   server: {
     port: 3001,
+    host: '0.0.0.0', 
     proxy: {
       // Proxy API requests to our Express server
       '/video_feed': 'http://localhost:3000',
